76b3af339a11d3c4c48f6fdb509e1a728073a828
Dominic Vicente <dvicente4482@sdsu.edu>
2026-05-06 22:50:15 -0700

    Fix Prisma 7 config and added early implementations of vercel: move directUrl to prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
   },
   datasource: {
     url: process.env.DATABASE_URL!,
+    directUrl: process.env.DIRECT_URL!,
   },
   migrations: {
     seed: 'npx tsx prisma/seed.ts',
